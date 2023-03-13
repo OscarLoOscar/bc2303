77b1a9eb1572e75de26f3b9bf2dc56735aa7afd4
Lo Oscar <lokongkitoscar@gmail.com>
2023-03-13 17:49:59 +0800

    update

--- week2/Exercise2/LessonExercise2/Cat.java
+++ week2/Exercise2/LessonExercise2/Cat.java
@@ -4,21 +4,23 @@ public class Cat extends Animal {
   // inherited
   String color;
 
-  public Cat() {
+  public Cat() { // constructor 1.
     super();
   }
-  public Cat(String color,String Name){
+
+  public Cat(String color, String Name) {// constructor 2.
     super(name);
     this.color = color;
   }
+
   public void setColor(String color) {
     this.color = color;
 
   }
 
   public static void main(String[] args) {
-    Cat cat = new Cat();
-    cat.setName("Kathy"); //1.繼承左ANIMAL
+    Cat cat = new Cat();  // Line 7，11 ； 所以有時唔洗入item，有時可以入
+    cat.setName("Kathy"); // 1.繼承左ANIMAL
     cat.setColor("Color");
 
     Cat cat2 = new Cat("Sally", "Yellow");
